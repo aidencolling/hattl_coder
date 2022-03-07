b9fd39da1cb8c4bbd3a891eef93b0c8830f1f69c
hattl-coder <74291162+aidencolling@users.noreply.github.com>
2022-03-07 12:20:43 +0200

    Add files via upload

---
+++ tarot/card_meanings.js
@@ -0,0 +1,51 @@
+// card meanings
+
+// Suits
+
+var wands = "The Wands cards are about action, initiative, invention, and energy. They help to guide how you move through your life, and advise when to take action and when to hold back.";
+var cups = "The Cups Tarot cards represent your emotions, intuition, and relationships. These cards can guide you in love, and help you grow through understanding and processing your highest and lowest feelings.";
+var pentacles = "The Pentacles Tarot cards are associated with your work and finances. These cards can answer questions about your long-term goals and your money, family, and health.";
+var swords = "The Swords cards are all about challenges. They can tell you when conflict and heartache are looming, and help you harness the strength of your own mind.";
+
+// numbers
+
+var ace = "Aces represent beginnings. They indicate initiative, drive, potential, and the earliest stages of an endeavor.";
+var two = "Twos carry messages of balance and dichotomy. When a Two arises in your Tarot reading, you cannot move forward until equilibrium is reached.";
+var three = "Threes are about communication and interactions. They indicate the influence that others have over your life, work, and emotions.";
+var four = "The four cards represent a break or rest period. In order to move forward, you need to stop now and contemplate where you've been, and where you're going.";
+var five = "The fives are cards of adversity. These cards indicate conflict, loss, and other negative experiences in your life that must be overcome.";
+var six = "Sixes represent growth -- overcoming challenges, leaving bad situations behind, and gaining a greater understanding of who you are right now.";
+var seven = "The seven cards teach that you must have faith in yourself and the universe. There are many things happening to you, but confidence, determination, and truth will see you through.";
+var eight = "Eights are cards of work and change. These cards tell you that the only way to get where you want to be is to change what you're doing right now.";
+var nine = "The nines represent fruition. Things are coming together, and you may or may not like what you see now.";
+var ten = "The tens are about final outcomes and the end of a current cycle. They carry messages about the rewards or consequences you'll experience for the amount of work you've put in.";
+var page = "The Page cards represent messages and beginnings. The Pages indicate a new phase where you know what you want to do, but not how to do it. These cards tell you to gather all the information you can before you take your next steps.";
+var knight = "The Knights are all about movement. You know where you're going, and the Knights tell you it's time to set your wheels in motion.";
+var queen = "The Queen cards are feminine messages of power, potential, and advice. These cards often advise you to seek the help of a wiser, more experienced person.";
+var king = "The Kings flex their muscles with authority and power. These cards tell you that you have what it takes to succeed, but you must believe in everything that you are. ";
+
+// Major Arcania
+// https://www.biddytarot.com/tarot-card-meanings/major-arcana/
+
+var the_fool = "Beginnings,\n innocence,\n spontaneity,\n\ a free spirit\n\n The Fool is numbered 0 – the number of unlimited potential – and so does not have a specific place in the sequence of the Tarot cards. The Fool can be placed either at the beginning of the Major Arcana or at the end. The Major Arcana is often considered The Fool’s journey through life and as such, he is ever present and therefore needs no number. \n On The Fool Tarot card, a young man stands on the edge of a cliff, without a care in the world, as he sets out on a new adventure. He is gazing upwards toward the sky (and the Universe) and is seemingly unaware that he is about to skip off a precipice into the unknown. Over his shoulder rests a modest knapsack containing everything he needs – which isn’t much (let’s say he’s a minimalist). The white rose in his left hand represents his purity and innocence. And at his feet is a small white dog, representing loyalty and protection, that encourages him to charge forward and learn the lessons he came to learn. The mountains behind The Fool symbolize the challenges yet to come. They are forever present, but The Fool doesn’t care about them right now; he’s more focused on starting his expedition.";
+var the_magician = "Manifestation,\n resourcefulness,\n power,\n inspired action\n The Magician card is numbered One – the number of new beginnings and opportunities – and associates with the planet of Mercury. He stands with one arm stretched upwards towards the Universe, and the other pointing down to the earth. His positioning represents his connection between the spiritual realms and the material realms. The Magician uses this relationship to create and manifest his goals in the physical realm. He is the conduit that converts energy into matter. The Magician’s robe is white, symbolising purity, and his cloak is red, representing worldly experience and knowledge \nOn the table in front of him are the four symbols of the Tarot suits – a cup, pentacle, sword and wand – each symbolising one of the four elements – water, earth, air and fire. It is also a sign that he has all the tools (and elements) he needs to manifest his intentions into being. Above his head is the infinity symbol, and around his waist is a snake biting its own tail – both of which signal that he has access to unlimited potential. And in the foreground is an array of foliage and flowers, symbolising the blossoming and fruition of his ideas and aspirations.";
+var the_high_priestess = "Intuition,\n sacred knowledge,\n divine feminine,\n the subconscious mind\n\n The High Priestess sits in front of a thin veil decorated with pomegranates. The veil represents the separate conscious and subconscious realms, the seen and the unseen, and serves to keep casual onlookers out. Only the initiated may enter. The pomegranates on the veil are a symbol of abundance, fertility and the divine feminine, and are sacred to Persephone who ate a pomegranate seed in the underworld and was forced to return every year.\n On either side of The High Priestess stand two pillars, marking the entrance to this sacred, mystical temple (also associated with the Temple of Solomon). One pillar is black with the letter B (Boaz, meaning ‘in his strength’) and the other is white with the letter J (Jachin, meaning ‘he will establish’). The black and white colors of the pillars symbolize duality – masculine and feminine, darkness and light – stating that knowledge and acceptance of duality are required to enter this sacred space. \n The High Priestess wears a blue robe with a cross on her chest and a horned diadem (or crown), both a symbol of her divine knowledge and her status as a divine ruler. In her lap, she holds a scroll with the letter TORA, signifying the Greater Law (according to A. E. Waite). It is partly covered, signifying that this sacred knowledge is both explicit and implicit, it will only be revealed when the student is ready to look beyond the material realm. The crescent moon at her feet symbolizes her connection with the divine feminine, her intuition and subconscious mind, and the natural cycles of the moon.";
+var the_empress = "";
+var the_emperor = "";
+var the_hierophant = "";
+var the_lovers = "";
+var the_chariot = "";
+var strength = "";
+var the_hermit = "";
+var wheel_of_fortune = "";
+var justice = "";
+var the_hanged_man = "";
+var death = "";
+var temperance = "";
+var the_devil = "";
+var the_tower = "";
+var the_star = "";
+var the_moon = "";
+var the_sun = "";
+var judgement = "";
+var the_world = "";
